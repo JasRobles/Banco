fff35f7ad677c08ae5a1028996610cbb5cbb7328
JasRobles <63456629+JasRobles@users.noreply.github.com>
2020-10-24 16:14:24 -0600

    conexion

---
+++ appBanco/appBanco/src/main/java/com/unab/edu/appbanco/Conexion/Conection.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.appbanco.Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author 14-ck0013lab
+ */
+public class Conection {
+     private Connection conexion;
+
+    public Conection () {
+
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost/appbanco", "root", "root");
+            System.out.println("Conexion exitosa");
+
+        } catch (Exception e) {
+            System.out.println("error" + e);
+        }
+
+    }
+    public Connection retornarConexion() {
+        return conexion;
+    }
+}
