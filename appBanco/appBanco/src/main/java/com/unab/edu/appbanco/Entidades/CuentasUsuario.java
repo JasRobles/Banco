200970361b273752b4eb147c40129591e6baa518
JasRobles <63456629+JasRobles@users.noreply.github.com>
2020-10-24 17:25:08 -0600

    actualizacion

---
+++ appBanco/appBanco/src/main/java/com/unab/edu/appbanco/Entidades/CuentasUsuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.appbanco.Entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 14-ck0013lab
+ */
+public class CuentasUsuario extends Usuario{
+    private int id;
+    private double saldo;
+    private int idUsuario;
+    private int transaccion;
+    private Date fecha;
+}
---
+++ appBanco/appBanco/src/main/java/com/unab/edu/appbanco/Entidades/Usuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.appbanco.Entidades;
+
+/**
+ *
+ * @author 14-ck0013lab
+ */
+public class Usuario extends TipoUsuario{
+    private int IdU;
+    private String usuario;
+    private String password;
+    private int tipoUsuario;
+}
